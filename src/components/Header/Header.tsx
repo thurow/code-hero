ed7ca8f9a6e919d0f2dd70701cdb0ab1c02a46aa
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2020-11-01 16:02:07 -0300

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Header.scss'
 import Logo from '../../logo.png'
 
 const Header = (): JSX.Element => {
@@ -8,6 +9,16 @@ const Header = (): JSX.Element => {
         <Link to="/">
           <img src={Logo} alt="Objective" width="96" />
         </Link>
+
+        <div className="main-header__info">
+          <div className="main-header__info--candidate">
+            <strong>Alexandre Frederico Thurow</strong>
+            <span>Teste de Front-end</span>
+          </div>
+          <div className="main-header__info--avatar">
+            <strong>AF</strong>
+          </div>
+        </div>
       </header>
     )
 }
